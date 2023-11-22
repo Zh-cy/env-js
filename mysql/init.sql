634f942ac8a972401997ba6ee386e10b855c5953
Zh-cy <chunyang26604@gmail.com>
2023-11-22 22:52:38 +0100

    adaption

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,5 +1,3 @@
-
-
 -- create table 'user'
 CREATE TABLE IF NOT EXISTS user (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -16,28 +14,27 @@ CREATE TABLE IF NOT EXISTS user (
 );
 
 -- insert data
-INSERT INTO user (first_name, last_name, email, password) VALUES
-('John', 'Doe', 'johndoe1@example.com', 'password123'),
-('Jane', 'Smith', 'janesmith@example.com', 'password123'),
-('Alice', 'Johnson', 'alicejohnson@example.com', 'password123'),
-('Bob', 'Brown', 'bobbrown@example.com', 'password123'),
-('Carol', 'Davis', 'caroldavis@example.com', 'password123'),
-('David', 'Miller', 'davidmiller@example.com', 'password123'),
-('Eve', 'Wilson', 'evewilson@example.com', 'password123'),
-('Frank', 'Moore', 'frankmoore@example.com', 'password123'),
-('Grace', 'Taylor', 'gracetaylor@example.com', 'password123'),
-('Henry', 'Anderson', 'henryanderson@example.com', 'password123'),
-('Isabel', 'Thomas', 'isabelthomas@example.com', 'password123'),
-('Jack', 'Jackson', 'jackjackson@example.com', 'password123'),
-('Kathy', 'White', 'kathywhite@example.com', 'password123'),
-('Louis', 'Harris', 'louisharris@example.com', 'password123'),
-('Megan', 'Martin', 'meganmartin@example.com', 'password123'),
-('Nathan', 'Garcia', 'nathangarcia@example.com', 'password123'),
-('Olivia', 'Martinez', 'oliviamartinez@example.com', 'password123'),
-('Paul', 'Robinson', 'paulrobinson@example.com', 'password123'),
-('Quincy', 'Clark', 'quincyclark@example.com', 'password123'),
-('Rachel', 'Rodriguez', 'rachelrodriguez@example.com', 'password123');
-
+INSERT INTO user (first_name, last_name, email, gender, password) VALUES
+('John', 'Doe', 'johndoe1@example.com', 'female', 'password123'),
+('Jane', 'Smith', 'janesmith@example.com', 'female', 'password123'),
+('Alice', 'Johnson', 'alicejohnson@example.com', 'unknown', 'password123'),
+('Bob', 'Brown', 'bobbrown@example.com', 'female', 'password123'),
+('Carol', 'Davis', 'caroldavis@example.com', 'male', 'password123'),
+('David', 'Miller', 'davidmiller@example.com', 'female', 'password123'),
+('Eve', 'Wilson', 'evewilson@example.com', 'male', 'password123'),
+('Frank', 'Moore', 'frankmoore@example.com', 'male', 'password123'),
+('Grace', 'Taylor', 'gracetaylor@example.com', 'female', 'password123'),
+('Henry', 'Anderson', 'henryanderson@example.com', 'female', 'password123'),
+('Isabel', 'Thomas', 'isabelthomas@example.com', 'male', 'password123'),
+('Jack', 'Jackson', 'jackjackson@example.com', 'unknown', 'password123'),
+('Kathy', 'White', 'kathywhite@example.com', 'female', 'password123'),
+('Louis', 'Harris', 'louisharris@example.com', 'female', 'password123'),
+('Megan', 'Martin', 'meganmartin@example.com', 'male', 'password123'),
+('Nathan', 'Garcia', 'nathangarcia@example.com', 'male', 'password123'),
+('Olivia', 'Martinez', 'oliviamartinez@example.com', 'male', 'password123'),
+('Paul', 'Robinson', 'paulrobinson@example.com', 'male', 'password123'),
+('Quincy', 'Clark', 'quincyclark@example.com', 'female', 'password123'),
+('Rachel', 'Rodriguez', 'rachelrodriguez@example.com', 'unknown', 'password123');
 
 ALTER USER 'admin'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'admin';
 FLUSH PRIVILEGES;
